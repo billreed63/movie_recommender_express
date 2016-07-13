d674ea639c51d39c11cd78fccddfb465be3e61ab
Brian Burns <bburns@us.ibm.com>
2016-07-13 09:45:54 -0400

    bluemix manifest and env vars

---
+++ sl_env.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+export JUPYTER_HOST=169.54.140.107
+export SMALL_DATASET=hdfs://10.122.193.195:8020/data/movielens/ml-latest-small
+export LARGE_DATASET=hdfs://10.122.193.195:8020/data/movielens/ml-latest
+export SPARK_MASTER=spark://10.122.193.195:7077
